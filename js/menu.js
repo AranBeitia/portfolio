6c5c38ccccb3687c61040366538db6628797d85a
AranBeitia <arantza.beitia@gmail.com>
2021-01-24 20:31:42 +0100

    styles

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,34 @@
-const content = document.getElementsByClassName('.main-content')
-scrollnav.init(content)
+// const content = document.getElementsByClassName('.main-content')
+// scrollnav.init(content)
 
-console.log(content)
+document.addEventListener('DOMContentLoaded', () => {
+  click()
+})
+
+const click = () => {
+  const item = document.getElementById('toggle')
+
+  item.classList.forEach( it => {
+    if(it === 'hidden') {
+      item.classList.remove('hidden')
+      item.classList.add('active')
+    }
+
+    if(it === 'active') {
+      item.classList.remove('active')
+      item.classList.add('hidden')
+    }
+  })
+
+  // if(item === 'hello') {
+
+  //   item.classList.remove('hello')
+  // }
+
+  // if (item === '') {
+  //   item.classList.add('hello')
+  // }
+
+
+  console.log(item)
+}
