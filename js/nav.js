71fb1a1d6268b26aa5378be4abc8a684bba301cd
AranBeitia <arantza.beitia@gmail.com>
2021-01-28 20:53:07 +0100

    menu nav

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,15 @@
 const button = document.querySelector('.navigation__button')
 const item = document.querySelector('.navigation__link')
 
+// const itemsNav = ['about', 'skills', 'portfolio', 'contact']
+// const itemNav = []
+// itemsNav.forEach(function(itemN){
+//   itemNav.push(document.getElementById(itemN))
+// })
+
+
 const toggleMenu = () => {
+  // console.log(itemNav)
   const background = document.querySelector('.navigation__background')
   const menu = document.querySelector('.navigation__nav')
 
